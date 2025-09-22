623c85526b6dae677930734434b8aed54d7c2c2f
osama <osamasaif242@gmail.com>
2025-09-22 21:26:04 +0300

    Make AsyncHandler and CustomError

--- server.js
+++ server.js
@@ -1,17 +1,33 @@
 import express from "express";
 import { config } from 'dotenv';
 import connectDb from "./utils/db.js";
+import cors from "cors";
 
+// Setup
 config();
 connectDb();
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
+app.use(cors());
 
+// Handle Errors
+app.use((err, req, res, next) => {
+    res.status(err.code || 500).json({
+        success: false,
+        message: err.message || 'Server Error',
+        errors: err.errors || null
+    });
+});
+
+
+// Routes
 app.get("/", (req, res) => {
   res.send("Welcome to the Restaurant Management System API");
 });
 
+
+// Start Server
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
 });
