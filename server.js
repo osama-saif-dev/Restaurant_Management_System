838ba1005ecbf4392c0fca408b48cc0542e58571
osama <osamasaif242@gmail.com>
2025-09-22 21:15:05 +0300

    add folders and run db server

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 import express from "express";
+import { config } from 'dotenv';
+import connectDb from "./utils/db.js";
 
+config();
+connectDb();
 const app = express();
 const PORT = process.env.PORT || 3000;
-
 app.use(express.json());
 
 app.get("/", (req, res) => {
